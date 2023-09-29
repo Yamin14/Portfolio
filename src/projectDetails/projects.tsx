65432eb681b1629d5b9ec54ebbdaef5f3dc50383
Yamin14 <syalikazmi@gmail.com>
2023-09-29 14:31:06 +0500

    Errors fixed

--- src/projectDetails/projects.tsx
+++ src/projectDetails/projects.tsx
@@ -4,7 +4,6 @@ import {titles} from './titles';
 import { github_links, linkedin_links } from "./links";
 
 //images
-const images = [];
 const numOfProjects = 7;
 
 for (let i = 0; i < numOfProjects; i++) {
@@ -14,7 +13,7 @@ for (let i = 0; i < numOfProjects; i++) {
 //object for each project
 export const projectsInfo = titles.map((t, i) => {
     return {
-        title: titles[i],
+        title: t,
         desc: descriptions[i],
         image: `../../public/Images/image (${i}).png`,
         github: github_links[i],
